4d10b41c6d2f2dc7b8c65deecc268398a85ba66f
xKarol <karolpiskorz8@gmail.com>
2022-01-22 19:34:57 +0100

    Fix progressbar ref

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -11,6 +11,7 @@ export default function ProgressBar({ value, className }) {
     <div className={`h-[4px] ${className}`}>
       <div
         className={`transition-[width] w-[0%] h-full bg-gradient-to-r from-[#28C2F4] to-[#8A2EC7]`}
+        ref={progressRef}
       ></div>
     </div>
   );
