4850056b83ca9ced29db4778d9a0850bd75d62c2
xKarol <karolpiskorz8@gmail.com>
2022-07-05 19:21:59 +0200

    Add avatar component bg color

--- components/Avatar.js
+++ components/Avatar.js
@@ -4,7 +4,7 @@ import DefaultAvatar from "../assets/images/default-avatar.jpg";
 export default function Avatar({ src, className }) {
   return (
     <div
-      className={`w-full h-full rounded-[50%] flex items-center justify-center overflow-hidden relative ${className}`}
+      className={`bg-gray-100 w-full h-full rounded-[50%] flex items-center justify-center overflow-hidden relative ${className}`}
     >
       <Image
         src={src ? src : DefaultAvatar}
