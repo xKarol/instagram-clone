0514b377fc47bb33b9e68f5433cc5ad2a0ebba67
xKarol <karolpiskorz8@gmail.com>
2022-08-05 16:56:50 +0200

    Fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,28 @@
-module.exports = {
-  setupFilesAfterEnv: ["./jest.setup.js"],
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+  ],
+  moduleNameMapper: {
+    "^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$":
+      "<rootDir>/__mocks__/fileMock.js",
+  },
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+  },
+  transformIgnorePatterns: [
+    "/node_modules/",
+    "^.+\\.module\\.(css|sass|scss)$",
+  ],
 };
+
+module.exports = createJestConfig(customJestConfig);
