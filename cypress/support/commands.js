b07f5ecd25ae2208dd1949c847cdb24299078117
xKarol <karolpiskorz8@gmail.com>
2022-09-16 14:54:25 +0200

    Add react-testing-library/cypress package

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -29,6 +29,7 @@ import "firebase/compat/auth";
 import "firebase/compat/database";
 import "firebase/compat/firestore";
 import { attachCustomCommands } from "cypress-firebase";
+import '@testing-library/cypress/add-commands'
 
 const fbConfig = {
   apiKey: Cypress.env("apiKey"),
