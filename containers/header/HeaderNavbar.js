61ad5b5681e5d315cda87e271ebd89022107dde0
xKarol <karolpiskorz8@gmail.com>
2022-09-18 15:11:37 +0200

    Splitted HeaderContainer to more files

---
+++ containers/header/HeaderNavbar.js
@@ -0,0 +1,55 @@
+import { FaRegCompass } from "react-icons/fa";
+import { FiSend } from "react-icons/fi";
+import { IoMdHeartEmpty } from "react-icons/io";
+import { MdHomeFilled, MdOutlineAddBox } from "react-icons/md";
+import Avatar from "../../components/avatar";
+import DropdownMenu from "../../components/dropdown";
+import { HeaderNavLink, HeaderNavList } from "../../components/header";
+import { useUserContext } from "../../context/UserContext";
+import HeaderProfileDropdownContainer from "./HeaderProfileDropdown";
+
+const HeaderNavbarContainer = ({ setShow }) => {
+  const {
+    user: { username, avatar },
+  } = useUserContext();
+
+  return (
+    <HeaderNavList>
+      <HeaderNavLink href="/" aria-label="home">
+        <MdHomeFilled />
+      </HeaderNavLink>
+      <HeaderNavLink aria-label="message">
+        <FiSend />
+      </HeaderNavLink>
+      <HeaderNavLink
+        aria-label="upload"
+        onClick={() => setShow(true)}
+        data-testid="add-post-btn"
+      >
+        <MdOutlineAddBox />
+      </HeaderNavLink>
+      <HeaderNavLink aria-label="explore">
+        <FaRegCompass />
+      </HeaderNavLink>
+      <HeaderNavLink aria-label="notifications" data-id="dropdown">
+        <IoMdHeartEmpty />
+      </HeaderNavLink>
+      <HeaderNavLink
+        aria-label="user profile"
+        className="w-[23px] h-[23px]"
+        data-id="dropdown"
+      >
+        <DropdownMenu items={<HeaderProfileDropdownContainer />}>
+          <Avatar
+            src={avatar}
+            className="pointer-events-none"
+            size={23}
+            alt={`${username}'s avatar`}
+          />
+        </DropdownMenu>
+      </HeaderNavLink>
+    </HeaderNavList>
+  );
+};
+
+export default HeaderNavbarContainer;
--- containers/header/index.js
+++ containers/header/index.js
@@ -1,29 +1,18 @@
 import { useState } from "react";
 import { useUserContext } from "../../context/UserContext";
-import { MdHomeFilled, MdOutlineAddBox } from "react-icons/md";
-import { FiSend } from "react-icons/fi";
-import { FaRegCompass } from "react-icons/fa";
-import { IoMdHeartEmpty } from "react-icons/io";
 import Logo from "../../components/logo";
-import Avatar from "../../components/avatar";
-import DropdownMenu from "../../components/dropdown";
 import {
   HeaderContainer as Container,
   HeaderAuth,
   HeaderAuthButton,
-  HeaderNavLink,
-  HeaderNavList,
   HeaderSearchBar,
 } from "../../components/header";
 import { PostUploadContainer } from "../post-upload";
-import HeaderProfileDropdownContainer from "./HeaderProfileDropdown";
+import HeaderNavbarContainer from "./HeaderNavbar";
 
 const HeaderContainer = ({ ...props }) => {
   const [show, setShow] = useState(false);
-  const {
-    loggedIn,
-    user: { username, avatar },
-  } = useUserContext();
+  const { loggedIn } = useUserContext();
 
   return (
     <>
@@ -31,41 +20,7 @@ const HeaderContainer = ({ ...props }) => {
         <Logo size={100} href="/" data-testid="logo" />
         <HeaderSearchBar data-testid="header-searchbar" />
         {loggedIn ? (
-          <HeaderNavList>
-            <HeaderNavLink href="/" aria-label="home">
-              <MdHomeFilled />
-            </HeaderNavLink>
-            <HeaderNavLink href="/" aria-label="message">
-              <FiSend />
-            </HeaderNavLink>
-            <HeaderNavLink
-              aria-label="upload"
-              onClick={() => setShow(true)}
-              data-testid="add-post-btn"
-            >
-              <MdOutlineAddBox />
-            </HeaderNavLink>
-            <HeaderNavLink href="/" aria-label="explore">
-              <FaRegCompass />
-            </HeaderNavLink>
-            <HeaderNavLink aria-label="notifications" data-id="dropdown">
-              <IoMdHeartEmpty />
-            </HeaderNavLink>
-            <HeaderNavLink
-              aria-label="user profile"
-              className="w-[23px] h-[23px]"
-              data-id="dropdown"
-            >
-              <DropdownMenu items={<HeaderProfileDropdownContainer />}>
-                <Avatar
-                  src={avatar}
-                  className="pointer-events-none"
-                  size={23}
-                  alt={`${username}'s avatar`}
-                />
-              </DropdownMenu>
-            </HeaderNavLink>
-          </HeaderNavList>
+          <HeaderNavbarContainer setShow={setShow} />
         ) : (
           <HeaderAuth>
             <HeaderAuthButton href="/sign-in">Log In</HeaderAuthButton>
