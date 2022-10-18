a1986a58bf4b4eeaf08df211741cfb0aba36fb61
xKarol <karolpiskorz8@gmail.com>
2022-10-18 14:43:44 +0200

    Moved lint-staged config to root dir

---
+++ lint-staged.config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = {
+  //   "**/*.(ts|tsx)": () => "npm run type-check",
+  "*.{js,jsx,ts,tsx}": (filenames) => [
+    `npx eslint --fix ${filenames.join(" ")}`,
+  ],
+
+  "*": (filenames) => `npm run prettier:fix ${filenames.join(" ")}`,
+};
