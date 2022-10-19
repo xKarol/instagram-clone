838f390ba01ff016a4034de5c6547523b000ca05
xKarol <karolpiskorz8@gmail.com>
2022-10-19 21:49:24 +0200

    Add more types in UserType

--- src/@types/user.ts
+++ src/@types/user.ts
@@ -1,7 +1,13 @@
+export type FollowType = { uid: string };
+
 export type UserType = {
   // random: number;
+  // avatarFileName: string;
+  avatar: string;
   fullName: string;
   username: string;
   email: string;
   uid: string;
+  followers?: FollowType[];
+  followings?: FollowType[];
 };
