90ebb2484ec23661fec5a7ba4d268b936e4fcb4d
xKarol <karolpiskorz8@gmail.com>
2022-10-21 12:13:19 +0200

    Fix LoadingScreen Props type

--- src/components/loading-screen/loading-screen.tsx
+++ src/components/loading-screen/loading-screen.tsx
@@ -2,7 +2,7 @@ import { BsInstagram } from "react-icons/bs";
 
 type Props = React.ComponentPropsWithoutRef<"div">;
 
-const LoadingScreen = ({ ...rest }): Props => {
+const LoadingScreen = ({ ...rest }: Props) => {
   return (
     <div
       className="bg-white w-screen h-screen flex items-center justify-center text-[50px] text-gray-200"
