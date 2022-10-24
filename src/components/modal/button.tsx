30b67fdcdf26bd7b69fbdad854f88259b9a5e3a5
xKarol <karolpiskorz8@gmail.com>
2022-10-24 11:44:03 +0200

    Add modal button types

--- src/components/modal/button.tsx
+++ src/components/modal/button.tsx
@@ -1,14 +1,16 @@
 import clsx from "clsx";
 
-const Button = ({ children, onClick, className }) => {
+type Props = React.ComponentPropsWithoutRef<"button">;
+
+const Button = ({ children, className, ...props }: Props) => {
   return (
     <button
       aria-label="modal button"
-      onClick={onClick}
       className={clsx(
         "p-[10px] w-full text-[14px] border border-transparent border-t-gray-200 hover:bg-gray-100",
         className
       )}
+      {...props}
     >
       {children}
     </button>
