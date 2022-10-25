81a80650e8af793dd38376cd9ea016f261cf5695
xKarol <karolpiskorz8@gmail.com>
2022-10-25 19:24:12 +0200

    Disabled nprogress eslint errors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/unbound-method */
 import Head from "next/head";
 import Router from "next/router";
